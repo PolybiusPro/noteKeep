9456493c8949d2e9d40b6f5334ce92e9a678e0fe
Wes Sitzes <weston.sitzes@gmail.com>
2020-09-28 22:55:25 -0400

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LockIcon from '@material-ui/icons/Lock';
+
+function Header(){
+  return <header>
+    <h1><LockIcon fontSize="medium" />noteKeep</h1>
+  </header>
+}
+
+export default Header;
